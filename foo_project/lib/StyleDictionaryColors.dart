6a0b073095b7b9a3098eb81ea81273098335ebc4
tom-sosedow <tom-sosedow@users.noreply.github.com>
2022-10-19 17:01:03 +0000

    [create-pull-request] automated change

--- foo_project/lib/StyleDictionaryColors.dart
+++ foo_project/lib/StyleDictionaryColors.dart
@@ -4,7 +4,7 @@
 //
 
 // Do not edit directly
-// Generated on Wed, 19 Oct 2022 16:52:26 GMT
+// Generated on Wed, 19 Oct 2022 17:01:02 GMT
 
 
 
@@ -13,8 +13,9 @@ import 'dart:ui';
 class StyleDictionaryColors {
   StyleDictionaryColors._();
 
-    static const colorBackground = Color(0xFFFFFFFF);
-    static const colorPrimary = Color(0xFFE7A134);
+    static const colorBackground = Color(0xFF2F2F2F);
+    static const colorDarkgrey = Color(0xFF2F2F2F);
+    static const colorPrimary = Color(0xFF801A93);
     static const colorSecondary = Color(0xFF8CEBFF);
     static const colorWhite = Color(0xFFFFFFFF);
 }
