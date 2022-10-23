63211680e3417fd0514b09b0198edb23bba983fa
tom-sosedow <tom-sosedow@users.noreply.github.com>
2022-10-23 09:32:51 +0000

    [create-pull-request] automated change

--- build/compose/StyleDictionaryColor.kt
+++ build/compose/StyleDictionaryColor.kt
@@ -1,7 +1,7 @@
 
 
 // Do not edit directly
-// Generated on Sun, 23 Oct 2022 07:47:27 GMT
+// Generated on Sun, 23 Oct 2022 09:32:51 GMT
 
 
 
@@ -12,11 +12,11 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.*
 
 object StyleDictionaryColor {
-  val colorBackground = Color(0xffffffff)
+  val colorBackground = Color(0xff2f2f2f)
   val colorDarkgrey = Color(0xff2f2f2f)
   val colorFontDark = Color(0xff101010)
   val colorFontLight = Color(0xffeaeaea)
-  val colorPrimary = Color(0xff801a93)
+  val colorPrimary = Color(0xff48a03d)
   val colorSecondary = Color(0xff8cebff)
   val colorWhite = Color(0xffffffff)
 }
--- foo_project/lib/StyleDictionaryColors.dart
+++ foo_project/lib/StyleDictionaryColors.dart
@@ -4,7 +4,7 @@
 //
 
 // Do not edit directly
-// Generated on Sun, 23 Oct 2022 07:47:27 GMT
+// Generated on Sun, 23 Oct 2022 09:32:51 GMT
 
 
 
@@ -13,11 +13,11 @@ import 'dart:ui';
 class StyleDictionaryColors {
   StyleDictionaryColors._();
 
-    static const colorBackground = Color(0xFFFFFFFF);
+    static const colorBackground = Color(0xFF2F2F2F);
     static const colorDarkgrey = Color(0xFF2F2F2F);
     static const colorFontDark = Color(0xFF101010);
     static const colorFontLight = Color(0xFFEAEAEA);
-    static const colorPrimary = Color(0xFF801A93);
+    static const colorPrimary = Color(0xFF48A03D);
     static const colorSecondary = Color(0xFF8CEBFF);
     static const colorWhite = Color(0xFFFFFFFF);
 }
