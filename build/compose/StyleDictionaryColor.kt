c4be0d1b4857d870b99e73aa493ef369b237c52b
tom-sosedow <tom-sosedow@users.noreply.github.com>
2022-10-23 07:47:28 +0000

    [create-pull-request] automated change

--- build/compose/StyleDictionaryColor.kt
+++ build/compose/StyleDictionaryColor.kt
@@ -1,7 +1,7 @@
 
 
 // Do not edit directly
-// Generated on Wed, 19 Oct 2022 17:01:02 GMT
+// Generated on Sun, 23 Oct 2022 07:47:27 GMT
 
 
 
@@ -12,8 +12,10 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.*
 
 object StyleDictionaryColor {
-  val colorBackground = Color(0xff2f2f2f)
+  val colorBackground = Color(0xffffffff)
   val colorDarkgrey = Color(0xff2f2f2f)
+  val colorFontDark = Color(0xff101010)
+  val colorFontLight = Color(0xffeaeaea)
   val colorPrimary = Color(0xff801a93)
   val colorSecondary = Color(0xff8cebff)
   val colorWhite = Color(0xffffffff)
